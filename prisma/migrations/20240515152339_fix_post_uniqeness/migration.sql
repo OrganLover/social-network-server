fa842831a2d58a4563f006316c1797ccfcac8457
OrganLover <yunusovdinar.yunusow@gmail.com>
2024-05-15 20:26:24 +0500

    🗃️ db: [prisma] исправление уникальности постов

---
+++ prisma/migrations/20240515152339_fix_post_uniqeness/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "disliked_posts_post_id_key";
+
+-- DropIndex
+DROP INDEX "disliked_posts_user_id_key";
+
+-- DropIndex
+DROP INDEX "liked_posts_post_id_key";
+
+-- DropIndex
+DROP INDEX "liked_posts_user_id_key";
